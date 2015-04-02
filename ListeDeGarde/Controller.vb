1c041f53dc28eb93bce7d4149f6dde407d20b4e3
Martin houle <sel4@videotron.ca>
2015-04-02 10:04:39 -0400

    Revert "commentedOut a monthtosheet structure"
    This reverts commit 96cb1db7db8dbc816e496634aaa29ca1ff85825a.

--- ListeDeGarde/Controller.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Controller
-
-    Public Sub New(aSheet As Excel.Worksheet, aMonth As ScheduleMonth)
-
-    End Sub
-End Class
--- ListeDeGarde/MyYearMonthPicker/YearMonthPicker.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class YearMonthPicker
-
-End Class
