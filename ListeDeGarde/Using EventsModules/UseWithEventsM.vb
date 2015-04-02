fb205fece72f92552c7ee913eb191859297ad7ab
Martin houle <sel4@videotron.ca>
2015-04-02 10:14:36 -0400

    Revert "Revert "commentedOut a monthtosheet structure""
    This reverts commit 1c041f53dc28eb93bce7d4149f6dde407d20b4e3.

---
+++ ListeDeGarde/Using EventsModules/UseWithEventsM.vb
@@ -0,0 +1,35 @@
+﻿'Module UseWithEventsSM
+'    '==================================================================
+'    'Demonstrates Using WithEvents for Event Handling
+'    '==================================================================
+
+'    Private WithEvents xlApp As Excel.Application
+'    Private xlBook As Excel.Workbook
+'    Private WithEvents xlSheet1 As Excel.Worksheet
+
+
+
+
+
+
+'    Public Sub UseWithEvents()
+
+'        'Start Excel and create a new workbook.
+'        xlApp = Globals.ThisAddIn.Application
+'        xlBook = Globals.ThisAddIn.Application.ActiveWorkbook
+'        ' xlBook.Windows(1).Caption = "Uses WithEvents"
+
+'        'Get references to the three worksheets.
+'        xlSheet1 = Globals.ThisAddIn.Application.ActiveSheet
+
+'        'Make Excel visible and give the user control.
+'        'xlApp.Visible = True
+'        'xlApp.UserControl = True
+
+
+
+'    End Sub
+
+
+
+'End Module
