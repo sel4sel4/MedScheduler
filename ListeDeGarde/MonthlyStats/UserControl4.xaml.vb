a688794929314ef40494db8f954cbb80193744a0
Martin houle <sel4@videotron.ca>
2015-04-23 22:16:42 -0400

    added untracked changes

---
+++ ListeDeGarde/MonthlyStats/UserControl4.xaml.vb
@@ -0,0 +1,13 @@
+﻿
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Public Class UserControl4
+
+
+    Public Sub loadarray(aCollection As Collection)
+
+        MyData.ItemsSource = aCollection
+
+
+    End Sub
+End Class
